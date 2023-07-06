6e3cded996f842d51161c985689a00f349a84bb3
desng5 <desng5@gmail.com>
2023-07-05 22:27:16 -0500

    updated AlertMessage styling

--- src/components/AlertMessage.tsx
+++ src/components/AlertMessage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { CategoryType } from "../types/category";
 
 type AlertMessageProps = {
@@ -11,5 +12,50 @@ export const AlertMessage = ({
   category,
   flashMessage,
 }: AlertMessageProps) => {
-  return <div>{message}</div>;
+  const [isVisible, setIsVisible] = useState(true);
+
+  const getAlertStyle = (category: CategoryType) => {
+    switch (category) {
+      case "primary":
+        return "bg-blue-500 text-white";
+      case "secondary":
+        return "bg-gray-500 text-white";
+      case "success":
+        return "bg-green-500 text-white";
+      case "danger":
+        return "bg-red-500 text-white";
+      case "warning":
+        return "bg-yellow-500 text-black";
+      case "info":
+        return "bg-indigo-500 text-white";
+      case "light":
+        return "bg-gray-200 text-black";
+      case "dark":
+        return "bg-gray-800 text-white";
+      default:
+        return "";
+    }
+  };
+
+  const handleClose = () => {
+    setIsVisible(false);
+    flashMessage(null, null);
+  };
+
+  return isVisible ? (
+    <div
+      className={`rounded p-4 ${getAlertStyle(
+        category
+      )} flex items-center justify-between`}
+      role="alert"
+    >
+      <span>{message}</span>
+      <button
+        onClick={handleClose}
+        className="ml-4 rounded border border-black bg-transparent px-4 py-2 font-semibold text-black hover:border-transparent hover:text-white"
+      >
+        X
+      </button>
+    </div>
+  ) : null;
 };

d280454a8f9bd1aa7c2906c1450534c2bdf3f3a3
desng5 <desng5@gmail.com>
2023-07-06 22:34:46 -0500

    Updated styling

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -43,24 +43,38 @@ export const Login = ({ logUserIn, flashMessage }: LoginProps) => {
   };
 
   return (
-    <div>
-      <h1>Login Here</h1>
-      <form onSubmit={handleFormSubmit}>
-        <label>Email</label>
-        <input
-          name="email"
-          value={user.email}
-          onChange={handleInputChange}
-          type="email"
-        />
-        <label>Password</label>
-        <input
-          name="password"
-          value={user.password}
-          onChange={handleInputChange}
-          type="password"
-        />
-        <button type="submit">Log In</button>
+    <div className="mx-auto flex w-fit flex-col items-center justify-center rounded-2xl border-4 border-double border-amber-50 bg-gradient-to-b from-amber-700 to-amber-950 px-6 py-20">
+      <h1 className="mb-16 text-3xl text-amber-50">Login Here</h1>
+      <form
+        className="grid place-items-end gap-y-8 text-xl text-amber-50"
+        onSubmit={handleFormSubmit}
+      >
+        <div>
+          <label className="mr-4">Email:</label>
+          <input
+            className="rounded-md px-4 py-1 text-amber-950"
+            name="email"
+            value={user.email}
+            onChange={handleInputChange}
+            type="email"
+          />
+        </div>
+        <div>
+          <label className="mr-4">Password:</label>
+          <input
+            className="rounded-md px-4 py-1 text-amber-950"
+            name="password"
+            value={user.password}
+            onChange={handleInputChange}
+            type="password"
+          />
+        </div>
+        <button
+          className="mt-16 place-self-center rounded-full border-2 border-amber-50 px-8 py-2 duration-200 hover:bg-amber-50 hover:text-amber-950"
+          type="submit"
+        >
+          Log In
+        </button>
       </form>
     </div>
   );
